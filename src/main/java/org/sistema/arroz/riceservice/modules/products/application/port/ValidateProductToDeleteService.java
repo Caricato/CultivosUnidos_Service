b4d513c8051878eb87cfc03c6fe591d406782b33
Juan Diego <caricato665@gmail.com>
2021-11-07 14:52:00 -0500

    fix: throwing exception for product

--- src/main/java/org/sistema/arroz/riceservice/modules/products/application/port/ValidateProductToDeleteService.java
+++ src/main/java/org/sistema/arroz/riceservice/modules/products/application/port/ValidateProductToDeleteService.java
@@ -2,8 +2,8 @@ package org.sistema.arroz.riceservice.modules.products.application.port;
 
 import lombok.RequiredArgsConstructor;
 import org.sistema.arroz.riceservice.hexagonal.UseCase;
-import org.sistema.arroz.riceservice.modules.producers.domain.ProducerNotValidToDeleteException;
 import org.sistema.arroz.riceservice.modules.products.application.port.in.ValidateProductToDeleteUseCase;
+import org.sistema.arroz.riceservice.modules.products.domain.ProductNotValidToDeleteException;
 import org.sistema.arroz.riceservice.modules.schedule.application.port.out.ValidateProductSchedulesPort;
 
 @UseCase
@@ -14,6 +14,6 @@ public class ValidateProductToDeleteService implements ValidateProductToDeleteUs
     @Override
     public void validateProduct(Long productId) {
         var result = validateProductSchedulesPort.validateProductSchedules(productId);
-        if (!result.isEmpty()) throw new ProducerNotValidToDeleteException(productId);
+        if (!result.isEmpty()) throw new ProductNotValidToDeleteException(productId);
     }
 }
